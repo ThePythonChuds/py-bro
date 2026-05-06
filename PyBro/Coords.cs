d272991df99b2b4980a99485a5fbbc69305513a8
Filip Andrei-Robert <filandrei4@protonmail.com>
2026-05-06 19:21:11 +0300

    feat: defined interfaces

---
+++ PyBro/Coords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PyBro
+{
+	public class Coords
+	{
+		public Coords(uint row, uint col)
+		{
+			Row = row;
+			Col = col;
+		}
+
+		public uint Row { get; set; }
+		public uint Col { get; set; }
+	}
+}
