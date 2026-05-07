0877f861e6b6fa4e8e1455034f73334bb1db9c60
minyuna.web <mateidenisa68@gmail.com>
2026-05-07 15:51:40 +0300

    fix: build errors

--- PyBro/IKeyboardMouseSubject.cs
+++ PyBro/IKeyboardMouseSubject.cs
@@ -1,6 +1,6 @@
 namespace PyBro {
     public interface IKeyboardMouseSubject {
-       public UpdateListeners(IKeyboardMouseEvent e);
-       public AddListener(IKeyboardMouseListener l); 
+       public void UpdateListeners(IKeyboardMouseEvent e);
+       public void AddListener(IKeyboardMouseListener l); 
     }
 }
