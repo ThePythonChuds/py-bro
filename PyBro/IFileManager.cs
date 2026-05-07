a6606e95ea37a2721bb3288a84e22f1ac48a19d6
Alexandru Pinzaru <alexandru-gabriel.pinzaru@student.tuiasi.ro>
2026-05-07 16:24:48 +0300

    feat: add interfaces

---
+++ PyBro/IFileManager.cs
@@ -0,0 +1,8 @@
+namespace PyBro {
+    public interface IFileManager {
+        public void CreateFile();// ?
+        public void RemoveFile();// ?
+        public void SaveFile();// ?
+        public string LoadFile();
+    }
+}
