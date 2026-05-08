a92c46bf374cab0072772ceadccc6329881c3ce8
Alexandru Pinzaru <alexandru-gabriel.pinzaru@student.tuiasi.ro>
2026-05-08 11:41:09 +0300

    feat: add implementation

---
+++ PyBro/Controller.cs
@@ -0,0 +1,10 @@
+namespace PyBro {
+    public class Controller {
+        public IPythonInterpreterController pythonInterpreterController;
+        public IBufferManagerController bufferManagerController;
+        public IConsoleController consoleController;
+        public IFileManagerController fileManagerController;
+
+        Controller();
+    }
+}
--- PyBro/IKeyboardMouseListener.cs
+++ PyBro/IKeyboardMouseListener.cs
@@ -2,4 +2,21 @@ namespace PyBro {
     public interface IKeyboardMouseListener {
         public void GetUpdate(IKeyboardMouseEvent e);
     }
+
+    public class KeyboardMouseListener : IKeyboardMouseListener {
+
+        public KeyboardMouseListener();
+        public void GetUpdate(IKeyboardMouseEvent e)
+        {
+            if (e.EType == EventType.KeyPressed)
+            {
+                // TODO: vedem
+            }
+
+            if(e.EType == EventType.MouseClick)
+            {
+                // TODO: vedem
+            }
+        }
+    }
 }
